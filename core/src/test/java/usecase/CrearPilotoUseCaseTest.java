256fafef2c280f1213a04aa84eedb84545af2696
Agustin <mumugomez19@gmail.com>
2025-10-14 18:59:44 -0300

    Use Case Completo

--- core/src/test/java/usecase/CrearPilotoUseCaseTest.java
+++ core/src/test/java/usecase/CrearPilotoUseCaseTest.java
@@ -1,5 +1,7 @@
 package usecase;
 
+import exception.ExceptionPilotoExiste;
+import exception.ExceptionRepositorio;
 import model.Piloto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,7 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CrearPilotoUseCaseTest {
@@ -32,5 +34,44 @@ public class CrearPilotoUseCaseTest {
 
         Assertions.assertTrue(resultado);
     }
+
+    @Test
+    public void crearPiloto_PilotoDuplicado(){
+
+        CrearPilotoUseCase crearPilotoUseCase=new CrearPilotoUseCase(guardarPilotoRepositorio);
+
+        when(guardarPilotoRepositorio.existe("12345678")).thenReturn(true);
+
+        ExceptionPilotoExiste exceptionPilotoExiste = Assertions.assertThrows(ExceptionPilotoExiste.class, () -> {
+            crearPilotoUseCase.crearPiloto(UUID.randomUUID(), "Franco", "Colapinto", "12345678", LocalDate.of(2001, 2, 5));
+        }, "El piloto ya existe");
+
+        verify(guardarPilotoRepositorio,never()).guardarPiloto(any(Piloto.class));
+    }
+
+    @Test
+    public void crearPiloto_AtributosInvalidos(){
+        CrearPilotoUseCase crearPilotoUseCase=new CrearPilotoUseCase(guardarPilotoRepositorio);
+
+        verify(guardarPilotoRepositorio,never()).existe(any());
+        verify(guardarPilotoRepositorio,never()).guardarPiloto(any());
+
+        Assertions.assertThrows(RuntimeException.class,()->crearPilotoUseCase.crearPiloto(UUID.randomUUID(),"","  ","12345678", LocalDate.of(2001,2,5))) ;
+
+    }
+
+    @Test
+    public void CrearPiloto_RepositorioFalla(){
+        CrearPilotoUseCase crearPilotoUseCase=new CrearPilotoUseCase(guardarPilotoRepositorio);
+
+        when(guardarPilotoRepositorio.existe("12345678")).thenReturn(false);
+        when(guardarPilotoRepositorio.guardarPiloto(any())).thenReturn(false);
+
+        Assertions.assertThrows(ExceptionRepositorio.class, ()->crearPilotoUseCase.crearPiloto(UUID.randomUUID(),"Franco","Colapinto","12345678",LocalDate.of(2002,2,5)));
+
+
+
+    }
+
 
 }
